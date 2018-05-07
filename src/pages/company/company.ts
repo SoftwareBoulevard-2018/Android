ea59e66833de88a520572ef4990479453c913896
victorcatn <vdcatanoc@unal.edu.co>
2018-05-07 14:56:51 -0500

    polish companies

--- src/pages/company/company.ts
+++ src/pages/company/company.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 
+import { NavController, NavParams } from 'ionic-angular';
 
-import {
-  NavController
-} from 'ionic-angular';
+import { Company } from '../../models/company';
 
 @Component({
   selector: 'company',
   templateUrl: 'company.html'
 })
 export class ViewCompanyPage {
-
+  company: Company;
   constructor(
     public navCtrl: NavController,
-  ) {}
+    public navParams: NavParams
+  ) {
+    this.company = navParams.get("c")
+  }
 
 }
