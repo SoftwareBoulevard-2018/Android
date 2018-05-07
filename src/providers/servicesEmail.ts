f267c8907c5b1c93e4a8f19626a808d38c4e805f
jsninoc <jsninoc@unal.edu.co>
2018-05-07 18:09:06 -0500

    Commit Module 5
    Adaptation with the users created by module 2

--- src/providers/servicesEmail.ts
+++ src/providers/servicesEmail.ts
@@ -6,119 +6,103 @@ import { Injectable } from '@angular/core';
 export class servicesEmail {
 
 
-    private emailsSent = [
-        {
-            "id": 15,
-            "subject": "We should change our time for the project",
-            "sender": "jupvillegasgo@company.edu.co",
-            "content": "Hello , I was checking our project and I think we need to estimate more time ",
-            "receiver": ["cmzapata@company.edu.co", "jsnino@company.edu.co", "jorgitopro@company.edu.co", "paula_001@company.edu.co"],
-            "date": '01/02/2019'
-        },
-        {
-            "id": 16,
-            "subject": "We should change our cost for the project",
-            "sender": "jupvillegasgo@company.edu.co",
-            "content": "Hello , I was checking our project and I think we need to estimate more cost",
-            "receiver": ["eealpalav@cidenet.edu.co", "esalvarezpi@unal.edu.co", "bandradea@unal.edu.co", "scardonac@unal.edu.co", "hdgilh@unal.edu.co"],
-            "date": '01/03/2019'
-        },
-        {
-            "id": 17,
-            "subject": "We should change our time for project",
-            "sender": "jupvillegasgo@company.edu.co",
-            "content": "Hello , I was checking our project and I think we need to estimate more time ",
-            "receiver": ["lfbustamantea@company.edu.co", "sarbelaezc@company.edu.co", "jmespinosag@company.edu.co", "vgaviriam@company.edu.co", "afcalderad@company.edu.co"],
-            "date": '01/04/2019'
-        },
-        {
-            "id": 18,
-            "subject": "We need an analyst for our company",
-            "sender": "jupvillegasgo@company.edu.co",
-            "content": "Hello ,  We need an analyst for our company. Just send your CV",
-            "receiver": ["darias@unal.edu.co", "ldpenal@unal.edu.co"],
-            "date": '01/05/2019'
-        }
-    ];
-
-
-    private emailsReceived =  [
+    private emails =  [
         {
             "id": 1,
             "subject": "I need a project manager in our team",
-            "sender": "garena_master22e3@company.edu.co",
-            "content": "Please guys, make sure to send to the email garena_master22e3@company.edu.co your CVS ",
-            "receiver": ["jupvillegasgo@company.edu.co", "jsnino@company.edu.co", "jorgitopro@company.edu.co", "paula_001@company.edu.co"],
+            "sender": "jjsernaco",
+            "content": "Please guys, make sure to send to the email jjsernaco@gmail.edu.co your CVS ",
+            "receiver": ["cmzapata", "dddavid", "judfernandez", "vdjaramillog"],
             "date": '01/07/2018'
         },
         {
             "id": 2,
             "subject": "Negotiations beetween companies",
-            "sender": "george_r33@company.edu.co",
-            "content": "Hello Guys, This is george for @company, We need to do some negotiations beetween companies in order to get better results in the next round",
-            "receiver": ["jupvillegasgo@company.edu.co", "esalvarezpi@unal.edu.co", "bandradea@unal.edu.co", "scardonac@unal.edu.co", "hdgilh@unal.edu.co"],
+            "sender": "judfernandez",
+            "content": "Hello Guys, I am is juan david and we need to do some negotiations beetween companies in order to get better results in the next round",
+            "receiver": ["afaguilarr", "cmzapata", "vdjaramillog", "dacalles", "dddavid"],
             "date": '02/07/2018'
         },
         {
             "id": 3,
             "subject": "Training questions",
-            "sender": "danielfmg@company.edu.co",
+            "sender": "dacalles",
             "content": "Hello , We need to do some training questions in order to answers those questions for instant project.   Good night. Att: Daniel ",
-            "receiver": ["lfbustamantea@company.edu.co", "sarbelaezc@company.edu.co", "jupvillegasgo@company.edu.co", "vgaviriam@company.edu.co", "afcalderad@company.edu.co"],
+            "receiver": ["cmzapata", "vdjaramillog", "dddavid", "judfernandez", "jpchavesm"],
             "date": '03/07/2018'
         },
         {
             "id": 4,
             "subject": "We need an analyst in our company",
-            "sender": "cmzapata@company.edu.co",
-            "content": "Hello ,  We need an analyst for our company. Just send your CV. Att: Andrea",
-            "receiver": ["darias@unal.edu.co", "ldpenal@unal.edu.co", "jupvillegasgo@company.edu.co"],
+            "sender": "afaguilarr",
+            "content": "Hello ,  We need an analyst for our company. Just send your CV. Att: Andres Felipe",
+            "receiver": ["judfernandez", "jpchavesm", "cmzapata"],
             "date": '04/07/2018'
         },
         {
             "id": 5,
             "subject": "Troubles with the account creation",
-            "sender": "dv007@company.edu.co",
+            "sender": "jpchavesm",
             "content": "Hello , I am getting some troubles with the creation of accounts guys. Sorry :(",
-            "receiver": ["darias@unal.edu.co", "jupvillegasgo@company.edu.co","ldpenal@unal.edu.co"],
+            "receiver": ["cmzapata", "dacalles","jjsernaco"],
             "date": '05/07/2018'
         },
         {
             "id": 6,
             "subject": "Troubles with bottons ",
-            "sender": "avicii@cidenet.co",
+            "sender": "dacalles",
             "content": "Hello , I am getting some troubles with the botton which list companies",
-            "receiver": ["andres@cidenet.co"],
+            "receiver": ["cmzapata"],
             "date": '08/07/2018'
         },
 
         {
-            "id": 6,
+            "id": 7,
             "subject": "We need developers in our company ",
-            "sender": "cmzapata@company.edu.co",
+            "sender": "vdjaramillog",
             "content": "Hello , I am Carlos and we need two developers with skills of Angular and NodeJs in our compnany",
-            "receiver": ["darias@unal.edu.co", "ldpenal@unal.edu.co", "jupvillegasgo@company.edu.co"],
+            "receiver": ["jjsernaco", "cmzapata", "dddavid"],
             "date": '05/07/2018'
         },
+        {
+            "id": 15,
+            "subject": "We should change our time for the project",
+            "sender": "judfernandez",
+            "content": "Hello , I was checking our project and I think we need to estimate more time ",
+            "receiver": ["afaguilarr", "jjsernaco", "dddavid", "dacalles"],
+            "date": '01/02/2019'
+        },
+        {
+            "id": 16,
+            "subject": "We should change our cost for the project",
+            "sender": "afaguilarr",
+            "content": "Hello , I was checking our project and I think we need to estimate more cost",
+            "receiver": ["judfernandez", "dddavid", "jjsernaco", "vdjaramillog", "cmzapata"],
+            "date": '01/03/2019'
+        },
+        {
+            "id": 17,
+            "subject": "We should change our time for project",
+            "sender": "vdjaramillog",
+            "content": "Hello , I was checking our project and I think we need to estimate more time ",
+            "receiver": ["jjsernaco", "afaguilarr", "judfernandez", "cmzapata", "dddavid"],
+            "date": '01/04/2019'
+        },
+        {
+            "id": 18,
+            "subject": "We need an analyst for our company",
+            "sender": "cmzapata",
+            "content": "Hello ,  We need an analyst for our company. Just send your CV",
+            "receiver": ["dacalles", "vdjaramillog"],
+            "date": '01/05/2019'
+        }
 
     ];
-    
-    constructor() { }
+ 
 
 
-    public getEmailsSent() {
-        return this.emailsSent;
-
-    }
-
-
-    public getEmailsReceived(){
-
-        return this.emailsReceived;
+    public getEmails() {
+        return this.emails;
 
     }
-
-
-
-
 }
