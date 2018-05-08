161e4bb688703aca3714eea55d9e2c265aab26dc
DavidChaMun <38385223+DavidChaMun@users.noreply.github.com>
2018-05-08 00:05:49 -0500

    Adding puzzle

--- src/pages/generate-resources/generate-resources.ts
+++ src/pages/generate-resources/generate-resources.ts
@@ -14,12 +14,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'generate-resources.html',
 })
 export class GenerateResourcesPage {
+  puzzlePieces: String[] = [];
+  puzzle: number[][] = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.selectPuzzle();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GenerateResourcesPage');
   }
+  
+  try()
+  {
+    alert("Clicked");
+  }
+
+  selectPuzzle()
+  {
+    var i: number;
+    for(i=0; i<16; i++)
+    {
+      var imgSrc: String = "./puzzles/puzzle1/".concat("result").concat(i.toString()).concat(".png");
+      this.puzzlePieces.push(imgSrc);
+      console.log(this.puzzlePieces[i]);
+    }
+
+    var suffle: number[] = [3, 2, 6, 10, 1, 4, 8, 11, 15, 5, 14, 13, 7, 9, 12, 0];
+    
+    var k = 0;
+    for(i = 0; i< 4; i++)
+    {
+      for(var j = 0; j < 4; j++)
+      {
+        this.puzzle[i][j] = suffle[k];
+        k++;
+      }
+    }
+
+  }
+
+
 
 }
