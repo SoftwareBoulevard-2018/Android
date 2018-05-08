602eef4d280db982e8d0c2312de06fc98ab3a345
David Ospina Osorio <dospinao@unal.edu.co>
2018-05-07 22:12:18 -0500

    errores corregidos
    elementos no usados y duplicados depurados

--- src/pages/set-up/set-up.ts
+++ src/pages/set-up/set-up.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ProjectlistPage } from '../set-up/projectlist/projectlist';
 
-import {
-  NavController
-} from 'ionic-angular';
-
 @Component({
   selector: 'set-up',
   templateUrl: 'set-up.html'
