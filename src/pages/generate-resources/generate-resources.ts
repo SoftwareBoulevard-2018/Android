95f0049ea7f8352b530cc3f9105133d6f8b2ae8e
DavidChaMun <38385223+DavidChaMun@users.noreply.github.com>
2018-05-24 23:12:08 -0500

    Added puzzle check button functionality

--- src/pages/generate-resources/generate-resources.ts
+++ src/pages/generate-resources/generate-resources.ts
@@ -40,7 +40,8 @@ export class GenerateResourcesPage {
       console.log(this.puzzlePieces[i]);
     }
     //this.puzzlePieces[15] = "";
-    var suffle: number[] = [3, 2, 6, 10, 1, 4, 8, 11, 15, 5, 14, 13, 7, 9, 12, 0];
+    //var suffle: number[] = [0, 3, 2, 6, 10, 1, 4, 8, 11, 5, 14, 13, 7, 9, 12, 15];
+    var suffle: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     
     var k = 0;
     for(i = 0; i< 4; i++)
@@ -55,7 +56,13 @@ export class GenerateResourcesPage {
   }
   sendPuzzle()
   {
-    alert("Wrong answer");
+    if(this.isOrdered())
+    {
+      alert("Right answer");
+    }else
+    {
+      alert("Wrong answer");
+    }
   }
 
   movePiece(row: number, col: number)
@@ -68,29 +75,47 @@ export class GenerateResourcesPage {
     //alert(row.toString().concat(col.toString()));
   }
 
+  private isOrdered():boolean
+  {
+    var order: number = 0;
+    for(var i = 0; i < 4; i++)
+    {
+      for(var j = 0; j < 4; j++)
+      {
+        if(!(this.puzzle[i][j] === order))
+        {
+          //alert("returned at".concat(order.toString()));
+          return false;
+        }
+        order++;
+      }
+    }
+    return true;
+  }
+
   private isAdjacent(row: number, col: number): boolean
   {
     if(row < 3 && col < 3 && row > 0 && col > 0)
     {
-      if(this.puzzle[row+1][col] === 0)
+      if(this.puzzle[row+1][col] === 15)
       {
         
         this.swappPiece(row, col, row+1, col); 
         return true;
       }
-      if(this.puzzle[row][col+1] === 0)
+      if(this.puzzle[row][col+1] === 15)
       {
         
         this.swappPiece(row, col, row, col+1); 
         return true;
       }
-      if(this.puzzle[row-1][col] === 0)
+      if(this.puzzle[row-1][col] === 15)
       {
         
         this.swappPiece(row, col, row-1, col); 
         return true;
       }
-      if(this.puzzle[row][col-1] === 0)
+      if(this.puzzle[row][col-1] === 15)
       {
         
         this.swappPiece(row, col, row, col-1); 
@@ -100,13 +125,13 @@ export class GenerateResourcesPage {
 
     if((row === 3 || row === 0) && (col < 3  && col > 0))
     {
-      if(this.puzzle[row][col+1] === 0)
+      if(this.puzzle[row][col+1] === 15)
       {
         
         this.swappPiece(row, col, row, col+1); 
         return true;
       }   
-      if(this.puzzle[row][col-1] === 0)
+      if(this.puzzle[row][col-1] === 15)
       {
         
         this.swappPiece(row, col, row, col-1); 
@@ -115,19 +140,19 @@ export class GenerateResourcesPage {
       
       if(row === 3 && col > 0 && col < 3)
       {
-        if(this.puzzle[row-1][col] === 0)
+        if(this.puzzle[row-1][col] === 15)
         {
           
           this.swappPiece(row, col, row-1, col); 
           return true;
         } 
-        if(this.puzzle[row][col+1] === 0)
+        if(this.puzzle[row][col+1] === 15)
         {
           
           this.swappPiece(row, col, row, col+1); 
           return true;
         } 
-        if(this.puzzle[row][col-1] === 0)
+        if(this.puzzle[row][col-1] === 15)
         {
           
           this.swappPiece(row, col, row, col-1); 
@@ -136,19 +161,19 @@ export class GenerateResourcesPage {
       }
       if(row === 0 && col > 0 && col < 3)
       {
-        if(this.puzzle[row+1][col] === 0)
+        if(this.puzzle[row+1][col] === 15)
         {
           
           this.swappPiece(row, col, row+1, col); 
           return true;
         } 
-        if(this.puzzle[row][col+1] === 0)
+        if(this.puzzle[row][col+1] === 15)
         {
           
           this.swappPiece(row, col, row, col+1); 
           return true;
         } 
-        if(this.puzzle[row][col-1] === 0)
+        if(this.puzzle[row][col-1] === 15)
         {
           
           this.swappPiece(row, col, row, col-1); 
@@ -160,13 +185,13 @@ export class GenerateResourcesPage {
 
     if((row < 3 && row > 0) && (col === 3 || col === 0))
     {
-      if(this.puzzle[row+1][col] === 0)
+      if(this.puzzle[row+1][col] === 15)
       {
         
         this.swappPiece(row, col, row+1, col); 
         return true;
       }   
-      if(this.puzzle[row-1][col] === 0)
+      if(this.puzzle[row-1][col] === 15)
       {
         
         this.swappPiece(row, col, row-1, col); 
@@ -174,19 +199,19 @@ export class GenerateResourcesPage {
       } 
       if(row < 3 && col === 3 && row > 0)
       {
-        if(this.puzzle[row][col-1] === 0)
+        if(this.puzzle[row][col-1] === 15)
         {
           
           this.swappPiece(row, col, row, col-1); 
           return true;
         }   
-        if(this.puzzle[row+1][col] === 0)
+        if(this.puzzle[row+1][col] === 15)
         {
           
           this.swappPiece(row, col, row+1, col); 
           return true;
         } 
-        if(this.puzzle[row-1][col] === 0)
+        if(this.puzzle[row-1][col] === 15)
         {
           
           this.swappPiece(row, col, row-1, col); 
@@ -195,19 +220,19 @@ export class GenerateResourcesPage {
       }
       if(row < 3 && col === 0 && row > 0)
       {
-        if(this.puzzle[row][col+1] === 0)
+        if(this.puzzle[row][col+1] === 15)
         {
           
           this.swappPiece(row, col, row, col+1); 
           return true;
         }   
-        if(this.puzzle[row+1][col] === 0)
+        if(this.puzzle[row+1][col] === 15)
         {
           
           this.swappPiece(row, col, row+1, col); 
           return true;
         } 
-        if(this.puzzle[row-1][col] === 0)
+        if(this.puzzle[row-1][col] === 15)
         {
           
           this.swappPiece(row, col, row-1, col); 
@@ -217,7 +242,7 @@ export class GenerateResourcesPage {
     }
     if((row === 3 || row === 0) && col === 0)
     {
-      if(this.puzzle[row][col+1] === 0)
+      if(this.puzzle[row][col+1] === 15)
       {
         
         this.swappPiece(row, col, row, col+1); 
@@ -225,7 +250,7 @@ export class GenerateResourcesPage {
       }
       if(row === 3)
       {
-        if(this.puzzle[row-1][col] === 0)
+        if(this.puzzle[row-1][col] === 15)
         {
           
           this.swappPiece(row, col, row-1, col); 
@@ -234,7 +259,7 @@ export class GenerateResourcesPage {
       }
       if(row === 0)
       {
-        if(this.puzzle[row+1][col] === 0)
+        if(this.puzzle[row+1][col] === 15)
         {
           
           this.swappPiece(row, col, row+1, col); 
@@ -244,7 +269,7 @@ export class GenerateResourcesPage {
     }
     if((row === 3 || row === 0) && col === 3)
     {
-      if(this.puzzle[row][col-1] === 0)
+      if(this.puzzle[row][col-1] === 15)
       {
         
         this.swappPiece(row, col, row, col-1); 
@@ -252,7 +277,7 @@ export class GenerateResourcesPage {
       }
       if(row === 3)
       {
-        if(this.puzzle[row-1][col] === 0)
+        if(this.puzzle[row-1][col] === 15)
         {
           
           this.swappPiece(row, col, row-1, col); 
@@ -261,7 +286,7 @@ export class GenerateResourcesPage {
       }
       if(row === 0)
       {
-        if(this.puzzle[row+1][col] === 0)
+        if(this.puzzle[row+1][col] === 15)
         {
           
           this.swappPiece(row, col, row+1, col); 
