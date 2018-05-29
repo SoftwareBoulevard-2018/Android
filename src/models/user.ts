e8906a02a20efcdfa47defe04b9c34504490c574
victorcatn <vdcatanoc@unal.edu.co>
2018-05-29 15:37:11 -0500

    added user page - module 2 finished

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,7 @@
 export class User {
-    creation_date: Date;
 
-    constructor(public id:string, public companyId?:string , public name?:string, public username?:string, 
-                public password?:string, public role?:string, public competencyLevel?: number, 
-                public questions_answered_right?: number, public questions_answered_wrong?: number, public resourcesSpent?: number) {
-      this.creation_date = new Date();
+    constructor(public id:string,public username:string,  public name?:string,  
+                public password?:string, public createdAt?:Date, public role?:string, public competencyLevel?: number, 
+                public correctTrainingQuestions?: number, public correctProjectQuestions?: number, public resourcesSpent?: number) {
     }
 }
