18e3594ac954ea4158f75b96b557df94a692d512
victorcatn <vdcatanoc@unal.edu.co>
2018-05-29 23:47:49 -0500

    added api selection

--- src/pages/edit-company/edit-company.ts
+++ src/pages/edit-company/edit-company.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, LoadingController, ToastController,NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 import { Company } from '../../models/company';
 import {HttpService} from '../../app/http.service';
@@ -19,30 +20,98 @@ export class EditCompanyPage {
   lacking_project_manager = true;
   project_managers;
   project_manager;
+  changedImage=false;
+  imageURI:any;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public httpService: HttpService
+    public httpService: HttpService,
+    private camera: Camera,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController
   ) {
     this.company = this.navParams.data.c;
     this.getCurrentProjectManager(this.company.id);
+  }
+
+  getImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.imageURI = imageData;
+      this.changedImage = true;
+    }, (err) => {
+      console.log(err);
+      this.presentToast(err);
+    });
+  }
+
+  uploadImage() {
+    let loader = this.loadingCtrl.create({
+      content: "Uploading image..."
+    });
+    loader.present();
+  
+    return this.httpService.uploadCompanyImage(this.imageURI)
+      .then((data) => {
+        this.company.image = data.response;
+      console.log(data+" Uploaded Successfully");
+      loader.dismiss();
+      this.presentToast("Image uploaded successfully");
+    }, (err) => {
+      console.log(err);
+      loader.dismiss();
+      this.presentToast(err);
+    });
+  }
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
   onSubmit(form: NgForm) {
     this.submitted = true;
 
     if (form.valid) {
-      return this.httpService.updateCompany(this.company, this.company.id).subscribe(() => {
-        if(this.project_manager!==undefined&&this.project_manager!==null&&this.project_manager!==''){
-          return this.httpService.updateUser({ companyId: this.company.id }, this.project_manager).subscribe(() => {
-            this.navCtrl.pop();          
+      if(this.changedImage){
+        this.uploadImage().then(() => {
+          this.httpService.updateCompany(this.company, this.company.id).subscribe(() => {
+            if(this.project_manager!==undefined&&this.project_manager!==null&&this.project_manager!==''){
+              return this.httpService.updateUser({ companyId: this.company.id }, this.project_manager).subscribe(() => {
+                this.navCtrl.pop();          
+              });
+            }else{
+              this.navCtrl.pop();
+            }
           });
-        }else{
-          this.navCtrl.pop();
-        }
-        
-      });
+        })
+      }else{
+        this.httpService.updateCompany(this.company, this.company.id).subscribe(() => {
+          if(this.project_manager!==undefined&&this.project_manager!==null&&this.project_manager!==''){
+            return this.httpService.updateUser({ companyId: this.company.id }, this.project_manager).subscribe(() => {
+              this.navCtrl.pop();          
+            });
+          }else{
+            this.navCtrl.pop();
+          }
+          
+        });
+      }
+      
       
     }
   }
