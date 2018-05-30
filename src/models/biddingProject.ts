8a892666b6e19c012162a9932693e9231460a187
santiquiroz <squirozu@unal.edu.co>
2018-05-30 12:55:38 -0500

    commit errors fixed

--- src/models/biddingProject.ts
+++ src/models/biddingProject.ts
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 export class BiddingProject {
   
   constructor(
@@ -15,27 +14,6 @@ export class BiddingProject {
     public  required_developer_level?: number,
     public required_tester_level?: number) 
 {}
-=======
-export class BiddingProject extends InstantProject{
-  time: number;
-  cost: number;
-  required_K: number;
-  required_analyst_level: number;
-  required_developer_level: number;
-  required_tester_level: number;
-  constructor(id?: string, name?: string,
-    numberOfDevelopingQuestionsPerAnalyst?:number,  numberOfDevelopingQuestionsPerTester?:number, 
-    numberOfDevelopingQuestionsPerDeveloper?:number,  rewarded_K?: number, time?: number, cost?: number, required_K?: number,
-              required_analyst_level?: number, required_developer_level?: number, required_tester_level?: number) {
-    super(id, name, numberOfDevelopingQuestionsPerTester, numberOfDevelopingQuestionsPerAnalyst, numberOfDevelopingQuestionsPerDeveloper, rewarded_K,);
-    this.time = time;
-    this.cost = cost;
-    this.required_K = required_K;
-    this.required_analyst_level = required_analyst_level;
-    this.required_developer_level = required_developer_level;
-    this.required_tester_level = required_tester_level;
-  }
->>>>>>> master
 }
 
 /*
