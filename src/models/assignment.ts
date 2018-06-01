0a0e36b5c2a3989ecffe4935ab9bb650e2c356dd
sechavarriap <970518@gmail.com>
2018-06-01 17:06:09 -0500

    corrected the assignment model according to the database structure

--- src/models/assignment.ts
+++ src/models/assignment.ts
@@ -1,15 +1,10 @@
 export class Assignment {
     assigment_id: string;
-    projectId: string;
-    questionId: string;
     project: string;
     question: string;
     state: string;
-    constructor(assigment_id?:  string, projectId?: string, questionId?: string, project?: string,question?: string,state?: string){
+    constructor(assigment_id?:  string, project?: string, question?: string, state?: string){
 		this.assigment_id = assigment_id;
-    this.projectId = projectId;
-
-    this.questionId = questionId;
 		this.project = project;
         this.question = question;
         this.state = state;
