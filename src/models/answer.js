2d70f8f08884930f683a17613948661a14ff3d92
victorcatn <vdcatanoc@unal.edu.co>
2018-06-01 18:27:19 -0500

    deleted garbage

--- src/models/answer.js
+++
@@ -1,9 +0,0 @@
-var Answer = /** @class */ (function () {
-    function Answer(description, veracity) {
-        this.description = description;
-        this.veracity = veracity;
-    }
-    return Answer;
-}());
-export { Answer };
-//# sourceMappingURL=answer.js.map
